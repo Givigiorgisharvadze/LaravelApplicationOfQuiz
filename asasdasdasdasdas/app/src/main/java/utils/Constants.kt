a249acd1dab2e7b15c6d4ff75e7403ba877274f3
Luka <luka.ivaniadze.2@btu.edu.ge>
2022-11-24 12:24:37 +0400

    finished project

---
+++ asasdasdasdasdas/app/src/main/java/utils/Constants.kt
@@ -0,0 +1,22 @@
+package utils
+
+import com.example.asasdasdasdasdas.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_face,
+    R.drawable.ic_flight,
+    R.drawable.ic_flower,
+    R.drawable.ic_gift,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+    R.drawable.ic_face,
+
+
+    )
